aab18b06a6593eb670192d045f922b8f2bf947ad
cofauver <cofauver@gmail.com>
2016-10-07 17:40:05 -0700

    adds a stack class solution

---
+++ stacks.rb
@@ -0,0 +1,51 @@
+require_relative 'singly_linked_list'
+
+class Stack
+  attr_reader :top, :contents
+
+  def initialize(top=nil)
+    @top = top
+    @contents = SinglyLinkedList.new(top)
+  end
+
+  def inspect
+    @contents.inspect.sub('(head)', '(top)')
+  end
+
+  def push (element)
+    @contents.insert_at_start(element)
+    @top = element
+  end
+
+  def pop
+    popped_element = @top
+    @contents.remove_from_start
+    popped_element
+  end
+
+  def empty?
+    @contents.empty?
+  end
+
+end
+
+
+def main
+  sta = Stack.new
+  p sta.inspect
+  sta.push 5
+  p sta.inspect
+  sta.push "fargo"
+  sta.push "minnetonka"
+  p sta.inspect
+  p sta.pop
+  p sta.inspect
+  p sta.empty?
+  sta.pop
+  sta.pop
+  sta.pop
+  p sta.inspect
+  p sta.empty?
+end
+
+main
