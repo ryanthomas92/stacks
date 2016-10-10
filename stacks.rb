c5e14530996d9ac4297b0307604a387bb82a0ffe
cofauver <cofauver@gmail.com>
2016-10-10 09:53:57 -0700

    removes solution

--- stacks.rb
+++ stacks.rb
@@ -4,48 +4,18 @@ class Stack
   attr_reader :top, :contents
 
   def initialize(top=nil)
-    @contents = SinglyLinkedList.new(top)
-    @top = @contents.head
   end
 
   def inspect
-    @contents.inspect.sub('(head)', '(top)')
   end
 
   def push (element)
-    @contents.insert_at_start(element)
-    @top = element
   end
 
   def pop
-    popped_element = @top
-    @contents.remove_from_start
-    popped_element
   end
 
   def empty?
-    @contents.empty?
   end
 
 end
-
-
-def main
-  sta = Stack.new
-  p sta.inspect
-  sta.push 5
-  p sta.inspect
-  sta.push "fargo"
-  sta.push "minnetonka"
-  p sta.inspect
-  p sta.pop
-  p sta.inspect
-  p sta.empty?
-  sta.pop
-  sta.pop
-  sta.pop
-  p sta.inspect
-  p sta.empty?
-end
-
-main
